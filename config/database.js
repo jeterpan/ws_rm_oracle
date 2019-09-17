5de71527c123aacc2957a53a5b9e327a228b0d71
jeterpan <jeter.costa@gmail.com>
2019-09-17 08:48:57 -0300

    Alterando string de conexao

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     hrPool: {
         user: 'rm',
         password: 'rm',
-        connectionString: '10.0.7.17/PRD',
+        connectionString: '192.168.0.120/PRODUCAO',
         poolMin: 10,
         poolMax: 10,
         poolIncrement: 0
