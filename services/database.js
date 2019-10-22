97cfb570dc000e6a87174c4057af068e294c5096
JeterPan <41131222+jeterpan@users.noreply.github.com>
2019-10-22 07:46:41 -0300

    updating pool name

--- services/database.js
+++ services/database.js
@@ -2,7 +2,7 @@ const oracledb = require('oracledb')
 const dbConfig = require('../config/database.js')
 
 async function initialize() {
-    const pool = await oracledb.createPool(dbConfig.hrPool)
+    const pool = await oracledb.createPool(dbConfig.rmPool)
 }
 
 module.exports.initialize = initialize
